6b644a2b0048becb488a861ee2c1ee6648b97963
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-08-08 22:05:25 +0200

    rename

---
+++ dlc2kinematics/__init__.py
@@ -0,0 +1,6 @@
+"""
+DLC2Kinematics
+©M. Mathis Labs
+"""
+
+from dlc2kinamtics.version import __version__
--- kinematik/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Kinematik
-© A. & M. Mathis Labs
-"""
-
-from kinematik.version import __version__
