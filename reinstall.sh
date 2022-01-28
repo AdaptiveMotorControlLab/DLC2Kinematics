04cd3aa4ffd1d21be05ca56ec64ff02878ee06a9
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-01-28 19:30:57 +0100

    getting ready...

--- reinstall.sh
+++ reinstall.sh
@@ -1,3 +1,3 @@
 pip uninstall dlc2kinamtics
 python3 setup.py sdist bdist_wheel
-pip install dist/dlc2kinamtics-0.0b1-py3-none-any.whl
+pip install dist/dlc2kinamtics-0.0.1-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 """
-DLC2Kinematics Package
-© M. Mathis Labs
-https://github.com/AdaptiveMotorControlLab/DLC2Kinematics/
-
+Kinematik
+© M. Mathis Lab
+https://github.com/AdaptiveMotorControlLab/kinematik/
 Please see AUTHORS for contributors.
-https://github.com/AdaptiveMotorControlLab/DLC2Kinematics/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
 """
 
 import setuptools
@@ -13,24 +12,44 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dlc2kinematics",
-    version="0.0b1",
-    author="Mackenzie Mathis & Lab",
+    name="kinematik",
+    version="0.0.1",
+    author="Mackenzie Mathis Lab Members",
     author_email="mackenzie@post.harvard.edu",
-    description="a post-DeepLabCut module for kinematic analysis",
+    description="Library for kinematic analysis of DeepLabCut outputs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AdaptiveMotorControlLab/DLC2Kinematics/",
-    install_requires=['h5py>=2.7','imageio>=2.3.0','intel-openmp',
-                      'ipython~=6.0.0','ipython-genutils==0.2.0',
-                      'matplotlib','moviepy','numpy','opencv-python',
-                      'pandas','patsy','python-dateutil','pyyaml>=5.1','requests',
-                      'ruamel.yaml>=0.15','setuptools','scikit-image','scikit-learn',
-                      'scikit-kinematics','scipy','six','statsmodels','tables',
-                      'tqdm>4','wheel'],
+    url="https://github.com/AdaptiveMotorControlLab/Kinematik/",
+    install_requires=[
+        "h5py",
+        "intel-openmp",
+        "ipython",
+        "ipython-genutils",
+        "matplotlib>=3.0.3",
+        "numpy>=1.14.5",
+        "pandas",
+        "python-dateutil",
+        "pyyaml",
+        "requests",
+        "setuptools",
+        "scikit-image",
+        "scikit-learn",
+        "scikit-kinematics",
+        "scipy",
+        "tables",
+        "umap",
+        "tqdm",
+        "ruamel.yaml",
+        "wheel",
+    ],
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-))
+    ),
+    entry_points="""[console_scripts]
+            kin=kin:main""",
+)
+
+# https://stackoverflow.com/questions/39590187/in-requirements-txt-what-does-tilde-equals-mean
