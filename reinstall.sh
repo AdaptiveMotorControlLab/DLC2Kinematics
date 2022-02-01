6fbea857b92b4edb14cea4ff092deffecd740368
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-02-01 17:30:57 +0100

    building wheels, ready for pypi

--- reinstall.sh
+++ reinstall.sh
@@ -1,3 +1,3 @@
-pip uninstall dlc2kinamtics
+pip uninstall dlc2kinematics
 python3 setup.py sdist bdist_wheel
-pip install dist/dlc2kinamtics-0.0.1-py3-none-any.whl
+pip install dist/dlc2kinematics-0.0.1-py3-none-any.whl
