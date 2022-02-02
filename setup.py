e90cc756c93b6b620b35feffc3d04ac0d8d7eafc
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-02-02 15:16:05 +0100

    header

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 """
-Kinematik
+dlc2kinematics
 © M. Mathis Lab
-https://github.com/AdaptiveMotorControlLab/kinematik/
-Please see AUTHORS for contributors.
-Licensed under GNU Lesser General Public License v3.0
+https://github.com/AdaptiveMotorControlLab/dlc2kinematics/
+
 """
 
 import setuptools
